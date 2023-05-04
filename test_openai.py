a971faf2d975b0a2ececac3a11e59ce64a9d00ab
silviazeng <zengxh2008@gmail.com>
2023-05-03 21:23:27 -0700

    add Dall-E2 with prompt from ChatGPT in test_openai.py: output .log and .jpg

--- test_openai.py
+++ test_openai.py
@@ -1,13 +1,33 @@
 import openai
+import requests
 
 API_KEY = open("my_key.txt" , "r").read()
 openai.api_key = API_KEY
 
-response = openai.ChatCompletion.create(
+#-----------------testing Chat API-----------------
+response_chat = openai.ChatCompletion.create(
 	model = "gpt-3.5-turbo",
 	messages = [
-		{"role": "user", "content": "blahblah"}
+		{"role": "user", "content": "provide a more detailed prompt for Dall-E based on this: an ultra happy assistant"}
 	]
 )
 
-print(response)
+response_chat_text = response_chat['choices'][0]['message']['content']
+
+with open("test_openai_prompt.log", "a") as file:
+		file.write(response_chat_text + "\n")
+
+
+#-----------------testing DallE API-----------------
+response_img = openai.Image.create(
+	prompt = response_chat_text,
+	n = 1,
+	size = "256x256"
+)
+
+image_url = response_img['data'][0]['url']
+
+img = requests.get(image_url)
+
+with open('test_openai_ultra_happy_assistant.jpg', 'wb') as f:
+	f.write(img.content)
