a76674eace2100c42d029e9c1be0d7b25e478f2d
xuezh2008 <xuezh2008@gmail.com>
2023-06-04 16:20:40 -0700

    try cuda config

--- test_image_understanding.py
+++ test_image_understanding.py
@@ -1,6 +1,8 @@
 from PIL import Image
 from transformers import AutoProcessor, Blip2ForConditionalGeneration
 import torch
+import os
+os.environ['PYTORCH_CUDA_ALLOC_CONF'] = 'cuda.conf'
 
 image = Image.open('test_openai_ultra_happy_assistant.jpg')
 
@@ -14,6 +16,6 @@ model.to(device)
 
 inputs = processor(image, return_tensors="pt").to(device, torch.float16)
 
-generated_ids = model.generate(**inputs, max_new_tokens=20)
+generated_ids = model.generate(**inputs, max_new_tokens=2)
 generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0].strip()
 print(generated_text)
